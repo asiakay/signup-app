9c98c5aaac5b3599d0454ae42c89b52358a04bfa
Asia K <asialakaygrady@gmail.com>
2022-05-06 02:18:43 -0400

    imported dependencies and middlewares

---
+++ server/server.js
@@ -0,0 +1,7 @@
+// Import dependencies
+const express = require('express')
+const bodyParser = require('body-parser')
+const compression = require('compression')
+const cors = require('cors')
+const helmet = require('helmet')
+
