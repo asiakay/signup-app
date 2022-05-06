89b006395bff8f92e005d7d4ef3d4c9fe2604b32
Asia K <asialakaygrady@gmail.com>
2022-05-06 02:01:25 -0400

    Added route for GET request to retrieve all signups

--- server/routes/signups-route.js
+++ server/routes/signups-route.js
@@ -6,3 +6,9 @@ const signupsRoutes = require('./../controllers/signups-controller.js')
 
 // Create router 
 const router = express.Router()
+
+// Add route for GET request to retrieve all signups
+// In server.js, signups route is specified as '/signups'
+// this means that '/all' translates to '/signups/all'
+router.get('/all', signupsRoutes.routesAll)
+
