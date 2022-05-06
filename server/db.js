e97a2c8a4ba004a0e50b79c2ab97a410ec7e945d
Asia K <asialakaygrady@gmail.com>
2022-05-06 01:11:10 -0400

    created users table in database

--- server/db.js
+++ server/db.js
@@ -12,3 +12,43 @@ const knex = require('knex')({
     },
     useNullAsDefault: true
 })
+
+// Create a table in the database called "books"
+knex.schema
+// Make sure no "signups" table exists
+  // before trying to create new
+    .hasTable('signups')
+        .then((exists) => {
+            if (!exists){
+        // If no "signups" table exists
+        // create new, with "id", "name", and "email" columns
+        // and use "id" as a primary identification
+        // and increment "id" with every new record (book)
+        return knex.schema.createTable('signups', (table) => {
+            table.increments('id').primary()
+            table.string('name')
+            table.string('email')
+        })
+        .then(() => {
+            // Log success message
+            console.log('Table \'Signups\' created')
+        })
+        .catch((error) => {
+            console.error(`There was an error creating table:  ${error}`)
+                })
+            }
+        })
+        .then(() => {
+            console.log('done')
+        })
+        .catch((error) => {
+            console.error(`There was an error setting up the database: ${error}`)
+        })
+// Just for debugging purposes:
+// Log all data in "signu" table
+knex.select('*').from('signups')
+        .then(data => console.log('data:', data))
+        .catch(err => console.log(err))
+
+// Export the database
+module.exports = knex
