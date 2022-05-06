aeeafabcff7f545b62b44ab3d314e6c6325f570e
Asia K <asialakaygrady@gmail.com>
2022-05-06 02:21:23 -0400

    set default port for express app

--- server/server.js
+++ server/server.js
@@ -8,3 +8,6 @@ const helmet = require('helmet')
 // import routes
 const signupsRouter = require('./routes/signups-route')
 
+// set default port for express app
+const PORT = process.env.PORT || 4001
+
