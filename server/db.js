c82bac4c882bb64ad61c84feb8d1b2e0569e072a
Asia K <asialakaygrady@gmail.com>
2022-05-06 00:48:21 -0400

    Created connection to SQLite database

--- server/db.js
+++ server/db.js
@@ -3,3 +3,12 @@ const path = require('path')
 
 // Get the location of database.sqlite file
 const dbPath = path.resolve(__dirname, 'db/database.sqlite')
+
+// Create connection to SQLite database
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: dbPath,
+    },
+    useNullAsDefault: true
+})
