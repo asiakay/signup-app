30a8a37bfd9d456d7e03ceb2ab17c01a0834aa63
Asia K <asialakaygrady@gmail.com>
2022-05-11 07:44:02 -0400

    updated links in Banner and h4 line-height

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,7 @@
 import React from "react";
 const Banner = () => {
     return ( <header>
-        <div className = "banner">
+        <div className = "banner"> 
         <div className = "container" >
         <div className = "row" >
         <div className = "col-lg-9 offset-lg-3 mx-auto" >
@@ -9,7 +9,7 @@ const Banner = () => {
         <h3 className = "" > {
             " "
         } {" "}
-        ASIALAKAY </h3> <h4> <a href="https://music-asiakayy.vercel.app/">audio</a>, <a href="https://artphotography-asiakayy.vercel.app/">photography</a>, <a href="https://algogems.io/gallery/AsiaK">nfts</a>, <a href="https://github.com/asiakay#resources">Curated Resources</a>, <a href="https://app.dopr.xyz/nft-directory">NFT Music Directory</a>, <a href="https://web3.career/">Web3 Jobs</a></h4> </div> </div> </div> </div> </div> </header>
+        ASIALAKAY </h3> <h4> <a href="https://music-asiakayy.vercel.app/">lyrics</a>, <a href="https://music-asiakayy.vercel.app/">vocals</a>, <a href="https://music-asiakayy.vercel.app/">songwriting</a>, <a href="https://music-asiakayy.vercel.app/">audio engineering</a>,  <a href="https://music-asiakayy.vercel.app/">sound design</a>, <a href="https://artphotography-asiakayy.vercel.app/">photography</a>, <a href="https://algogems.io/gallery/AsiaK">nfts</a>, <a href="https://github.com/asiakay#resources">curated resources</a>, <a href="https://app.dopr.xyz/nft-directory">nft music directory</a>, <a href="https://web3.career/">web3 jobs</a></h4> </div> </div> </div> </div> </div> </header>
     );
 };
 export default Banner;
