99252a5b93c2c984c28734586084d90ad2cdfda6
Asia K <asialakaygrady@gmail.com>
2022-05-12 08:50:19 -0400

    added links to documentation site

--- src/Banner.js
+++ src/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
         <h3 className = "" > {
             " "
         } {" "}
-        ASIALAKAY </h3> <h4> <a href="https://music-asiakayy.vercel.app/">lyrics</a>, <a href="https://music-asiakayy.vercel.app/">vocals</a>, <a href="https://music-asiakayy.vercel.app/">songwriting</a>, <a href="https://music-asiakayy.vercel.app/">audio engineering</a>,  <a href="https://music-asiakayy.vercel.app/">sound design</a>, <a href="https://artphotography-asiakayy.vercel.app/">photography</a>, <a href="https://algogems.io/gallery/AsiaK">nfts</a>, <a href="https://github.com/asiakay#resources">curated resources</a>, <a href="https://app.dopr.xyz/nft-directory">nft music directory</a>, <a href="https://web3.career/">web3 jobs</a></h4> </div> </div> </div> </div> </div> </header>
+        ASIALAKAY </h3><h4><a href="https://music-asiakayy.vercel.app/">audio engineering</a>, <a href="https://asialakay-docs-asiakayy.vercel.app/">dev</a>, <a href="https://asialakay-docs-asiakayy.vercel.app/">tutorials</a>, <a href="https://music-asiakayy.vercel.app/">lyrics</a>, <a href="https://music-asiakayy.vercel.app/">vocals</a>, <a href="https://music-asiakayy.vercel.app/">songwriting</a>,   <a href="https://music-asiakayy.vercel.app/">sound design</a>, <a href="https://artphotography-asiakayy.vercel.app/">photography</a>, <a href="https://algogems.io/gallery/AsiaK">nfts</a>, <a href="https://github.com/asiakay#resources">curated resources</a>, <a href="https://app.dopr.xyz/nft-directory">nft music directory</a>, <a href="https://web3.career/">web3 jobs</a>, <a href="https://asialakay-docs-asiakayy.vercel.app/">technical writing</a> </h4> </div> </div> </div> </div> </div> </header>
     );
 };
 export default Banner;
