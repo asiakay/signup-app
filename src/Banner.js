150dff030cd746574ef370ab3de18523b40861e6
Asia K <asialakaygrady@gmail.com>
2022-05-16 23:42:05 -0400

    added roundedness to sections and button

--- src/Banner.js
+++ src/Banner.js
@@ -8,15 +8,18 @@ import MixcloudEmbed from "./components/MixcloudEmbed";
 const Banner = () => {
     return ( <header>
 <Navigation />
+        
         <div className = "banner"> 
         <div className = "container" >
         <div className = "row" >
         <div className = "col-lg-9 offset-lg-3 mx-auto" >
         <div className = "caption-text text-center" >
+        
         <h3 className = "" > {
             " "
         } {" "}
         ASIALAKAY </h3>
+       
         
         <h4>
             Science. Technology. Art. Nature. Design. 
--- src/Signup.js
+++ src/Signup.js
@@ -69,7 +69,7 @@ const handleServerResponse = (ok, msg) => {
 
   return (
     <section>
-    <div className="container">
+    <div className="container" id="form">
         <div className="row">
         <div className="col-lg-7 offset-lg-2 g-0 mx-auto">
             <div className="fill-form form-box">
