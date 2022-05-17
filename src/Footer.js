60ada512487fa2d293e46060d57bcf5ddcc16bee
Asia K <asialakaygrady@gmail.com>
2022-05-17 01:29:21 -0400

    added bootstrap sticky footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,12 @@ import React from "react";
 const Footer = () => {
     return ( <footer className = "d-block" >
         
-        
-
+        <nav class="navbar fixed-bottom bg-light">
+<div class="container-fluid">
         <p className = "text-center" > © 2022 ASIALAKAY.NET </p>
         <p className="text-center"><a href="https://github.com/asiakay">github</a> | <a href="https://www.linkedin.com/in/asia-lakay-grady-669762b3/">linkedin</a> | <a href="https://twitter.com/asialakay">twitter</a></p>
+        </div>
+        </nav>
  </footer>
     );
 };
