ff278dd9fcfb3a43459e518d310884b01759bbea
Asia K <66960776+asiakay@users.noreply.github.com>
2023-01-16 14:44:56 -0500

    Update Banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -26,17 +26,19 @@ const { t } = useTranslation();
        {t("app_greet")} </h3>
 <p class="about">
 Welcome to my professional portfolio, where I showcase my experience and skills in the field of fullstack development, IT/tech project management, and Visual UX/UI. I have honed my abilities over 10 years of experience, and specialize in full stack development, project management, and blockchain development. My portfolio is a collection of my finest works, including web design, graphic design, digital marketing, multimedia art, audio composition, front-end development engineering, and creative technology, My tech stack includes the following languages, tools and frameworks: Music Theory & Sound Synthesis, Ableton Live - ProTools - Logic, HTML5/CSS3/JavaScript, Responsive Web Design, React UI Frameworks, Node.js & Express.js, Adobe Suite & Figma, Git - VCS - CI/CD, API Orchestration.
+<br/><br/>
 
 I am a strong believer in user-centered design, and my goal is to create visually appealing designs that effectively communicate the client's message while also being easy to use and navigate. I work in close collaboration with my clients to understand their specific needs and tailor my services to meet those needs.
-
+<br/><br/>
 I am passionate about my work and constantly strive to learn and grow as a professional. I hope that my portfolio will provide an insight into my skills and experience and how they can be of value to your organization.
-
+<br/><br/>
 I welcome the opportunity to discuss any potential projects or collaborations, and I look forward to hearing from you.
-
+<br/><br/>
 Thank you for visiting my portfolio.
 
 </p>
-    <p> 
+   {/*
+<p> 
     <ListGroup variant="flush" id="lg">
     <ListGroup.Item id="li">Music Theory & Sound Synthesis</ListGroup.Item>
     <ListGroup.Item id="li">Ableton Live - ProTools - Logic</ListGroup.Item>
@@ -49,6 +51,8 @@ Thank you for visiting my portfolio.
     <ListGroup.Item id="li">API Orchestration</ListGroup.Item>
     </ListGroup>
 </p>
+*/}
+
 {/* <Chatbot 
     config={config}
     messageParser={MessageParser}
